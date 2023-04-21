9ce31e36975df1571f7628a50ea1d33a56c08d25
Ju4nM <jenriquemflores@gmail.com>
2023-04-20 19:53:22 -0700

    Adicion de SplashScreen

---
+++ JET.Android/SplashScreen.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JET.Droid.Resources.values
+{
+    [Activity(Label = "JET", Icon = "@mipmap/icon", Theme = "@style/nuevoTema", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize)]
+    public class SplashScreen : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+
+            // Create your application here
+        }
+    }
+}
